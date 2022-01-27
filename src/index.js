1a8de2ffb30fdae14621cd0b3564e82213d44dbe
Oleksandr S <oleksandr.salnykov@valtech.com>
2022-01-27 17:38:06 +0200

    Adds script file

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const counterRef = document.querySelector('p');
+const btnRef = document.querySelector('button');
+let counter = 0;
+
+btnRef.addEventListener('click', () => {
+  counter++;
+  counterRef.textContent = counter;
+});
