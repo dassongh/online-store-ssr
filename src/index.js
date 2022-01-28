f13e31816b1706e1c9c59f8e068116bb7c823507
Alexander S <oleksandr.slv@gmail.com>
2022-01-28 17:15:32 +0200

    Moved to frontend folder

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const counterRef = document.querySelector('p');
-const btnRef = document.querySelector('button');
-let counter = 0;
-
-btnRef.addEventListener('click', () => {
-  counter++;
-  counterRef.textContent = counter;
-});
