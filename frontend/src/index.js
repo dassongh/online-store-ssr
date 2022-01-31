53a841ab9141cc468979d5e052b106670a0f8ead
Alexander S <oleksandr.slv@gmail.com>
2022-01-31 16:49:30 +0200

    Setting up webpack

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,17 @@
 import { test } from './variables';
 import json from './assets/json';
+import './babel';
+
+import xml from './assets/data.xml';
+
+import logo from './assets/valtech-logo.png';
 
 import './styles/styles.css';
+import './styles/sass.scss';
 
 console.log(test);
+console.log(logo);
+console.log('XML', xml);
 
 const counterRef = document.querySelector('p');
 const btnRef = document.querySelector('button');
