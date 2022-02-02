4e00d5bb98f31f5ac60257780755fc02137e4c00
Alexander S <oleksandr.slv@gmail.com>
2022-02-02 20:58:23 +0200

    finalizing hp layout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,6 @@
+import './js/owl.carousel.min.js';
+import './js/owlCarousel';
+
 import './styles/main.scss';
+import './styles/owlCarousel/owl.carousel.min.css';
+import './styles/owlCarousel/owl.theme.default.min.css';
