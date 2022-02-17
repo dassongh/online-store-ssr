78d05e09ea41160d091958b68bf9a9eab7cf5499
Alexander S <oleksandr.slv@gmail.com>
2022-02-17 17:57:27 +0200

    initing express app

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+import './js/owl.carousel.min.js';
+import './js/owlCarousel';
+import './js/mobile-menu';
+import './js/dropDownMenu';
+import './js/shopCart';
+
+import './styles/main.scss';
+import './styles/owlCarousel/owl.carousel.min.css';
+import './styles/owlCarousel/owl.theme.default.min.css';
